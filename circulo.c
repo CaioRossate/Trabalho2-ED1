baedb24b22558ce34010a3e514a92206181eca0e
CaioRossate <caiocaioedu@gmail.com>
2025-12-01 18:45:19 -0300

    primeiro commit

---
+++ circulo.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define M_PI 3.14159265358979323846
+#include "circulo.h"
+
+typedef struct {
+    int id;
+    float x, y, raio;
+    char *corP;
+    char *corB;
+} Circulo;
+
+CIRCULO cria_circulo(int id, float x, float y, float raio, char* corP, char* corB) {
+    Circulo* c = malloc(sizeof(Circulo));
+    if (c == NULL) {
+        printf("Erro ao alocar memoria para a criação do círculo");
+        exit(1);
+    }
+
+    c->id = id;
+    c->x = x;
+    c->y = y;
+    c->raio = raio;
+
+    c->corP = malloc(strlen(corP) + 1);
+    if (c->corP == NULL) {
+        printf("Erro ao alocar memória para a cor de preenchimento");
+        exit(1);
+    }
+    strcpy(c->corP, corP);
+
+    c->corB = malloc(strlen(corB) + 1);
+    if (c->corB == NULL) {
+        printf("Erro ao alocar memória para a cor da borda");
+        exit(1);
+    }
+    strcpy(c->corB, corB);
+
+    return c;
+}
+
+void destroiCirculo(CIRCULO c) {
+    Circulo* circ = (Circulo*)c;
+    if (circ != NULL) {
+        if (circ->corP != NULL) {
+            free(circ->corP);
+        }
+        if (circ->corB != NULL) {
+            free(circ->corB);
+        }
+        free(circ);
+    }
+}
+
+int getIDCirculo(CIRCULO c) {
+    return ((Circulo*)c)->id;
+}
+
+float getXCirculo(CIRCULO c) {
+    return ((Circulo*)c)->x;
+}
+
+float getYCirculo(CIRCULO c) {
+    return ((Circulo*)c)->y;
+}
+
+float getRaioCirculo(CIRCULO c) {
+    return ((Circulo*)c)->raio;
+}
+
+float getAreaCirculo(CIRCULO c) {
+    Circulo* circ = (Circulo*)c;
+    return M_PI * circ->raio * circ->raio;
+}
+
+char* getCorBCirculo(CIRCULO c) {
+    return ((Circulo*)c)->corB;
+}
+
+char* getCorPCirculo(CIRCULO c) {
+    return ((Circulo*)c)->corP;
+}
+
+void setIDCirculo(CIRCULO c, int id) {
+    ((Circulo*)c)->id = id;
+}
+
+void setXCirculo(CIRCULO c, float x) {
+    ((Circulo*)c)->x = x;
+}
+
+void setYCirculo(CIRCULO c, float y) {
+    ((Circulo*)c)->y = y;
+}
+
+void setRaioCirculo(CIRCULO c, float raio) {
+    ((Circulo*)c)->raio = raio;
+}
+
+void setCorBCirculo(CIRCULO c, char* corB) {
+    Circulo* circ = (Circulo*)c;
+    free(circ->corB);
+    circ->corB = malloc(strlen(corB) + 1);
+    strcpy(circ->corB, corB);
+}
+
+void setCorPCirculo(CIRCULO c, char* corP) {
+    Circulo* circ = (Circulo*)c;
+    free(circ->corP);
+    circ->corP = malloc(strlen(corP) + 1);
+    strcpy(circ->corP, corP);
+}
---
+++ circulo.h
@@ -0,0 +1,163 @@
+#ifndef CIRCULO_H
+#define CIRCULO_H
+
+/**
+ * @file circulo.h
+ * @brief Definições do TAD Círculo (formato em SVG).
+ * 
+ * @details
+ * O arquivo circulo.h declara a interface de um TAD, que representa um círculo
+ * em um plano bidimensional. Cada círculo possui um identificador, coordenadas (x,y),
+ * raio e cores de preenchimento e borda representadas como strings.
+ */ 
+ 
+ typedef void* CIRCULO;
+ 
+/**
+ * @param id identificador.
+ * @param x coordenada x do círculo.
+ * @param y coordenada y do círculo.
+ * @param raio raio do círculo.
+ * @param corP cor de preenchimento do círculo.
+ * @param corB cor da borda do círculo.
+ * 
+ * @return retorna um ponteiro para "CIRCULO".
+ * 
+ * 
+*/
+
+
+CIRCULO cria_circulo(int id, float x, float y, float raio, char* corP, char* corB);
+
+/**
+ * @brief Faz o ID do círculo.
+ * 
+ * @param c o círculo que vai possuir o ID.
+ * @return O id da forma.  
+ * 
+*/
+
+int getIDCirculo(CIRCULO c);
+
+/**
+ * @brief Faz o valor da coordenada X do círculo.
+ * 
+ * @param c O círculo que vai possuir a coordenada X.
+ * @return A coordenada X da forma.  
+ * 
+*/
+
+float getXCirculo(CIRCULO c);
+
+/**
+ * @brief Faz o valor da coordenada Y do círculo.
+ * 
+ * @param c O circulo que vai possuir a coordenada Y.
+ * @return A coordenada Y da forma.  
+ * 
+*/
+
+float getYCirculo(CIRCULO c);
+
+/**
+ * @brief Faz o valor do raio do círculo.
+ * 
+ * @param c O circulo que vai possuir o valor do raio.
+ * @return O raio da forma.  
+ * 
+*/
+
+float getRaioCirculo(CIRCULO c);
+
+/**
+ * @brief Faz a cor de preenchimento do círculo.
+ * 
+ * @param c O circulo que vai possuir a cor de preenchimento.
+ * @return String da cor de preenchimento da forma.  
+ * 
+*/
+
+char* getCorPCirculo(CIRCULO c);
+
+/**
+ * @brief Faz a cor da borda do círculo.
+ * 
+ * @param c O circulo que vai possuir a cor da borda.
+ * @return String da cor da borda da forma.  
+ * 
+*/
+
+char* getCorBCirculo(CIRCULO c);
+
+/**
+ * @brief Faz o calculo da área do círculo.
+ * 
+ * @param c A area do circulo.
+ * @return O valor da área da forma.  
+ * 
+*/
+
+float getAreaCirculo(CIRCULO c);
+
+/**
+ * @brief Define o ID do círculo.
+ * 
+ * @param c o círculo com o ID definido.
+ * @param id Novo ID definido.
+ */
+
+void setIDCirculo(CIRCULO c, int id);
+
+/**
+ * @brief Define a coordenada X do círculo.
+ * 
+ * @param c o círculo com a coordenada X definido.
+ * @param x Novo valor da coordenada X definido.
+ */
+
+
+void setXCirculo(CIRCULO c, float x);
+
+/**
+ * @brief Define a coordenada Y do círculo.
+ * 
+ * @param c o círculo com a coordenada Y definido.
+ * @param y Novo valor da coordenada Y definido.
+ */
+
+void setYCirculo(CIRCULO c, float y);
+
+/**
+ * @brief Define o raio do círculo.
+ * 
+ * @param c o círculo com o raio definido.
+ * @param raio Novo valor do raio definido.
+ */
+
+void setRaioCirculo(CIRCULO c, float raio);
+
+/**
+ * @brief Define a cor de preenchimento do círculo.
+ * 
+ * @param c o círculo com a cor de preencimento definido.
+ * @param corP Novo valor para a cor de preenchimento definido.
+ */
+
+void setCorPCirculo(CIRCULO c, char* corP);
+
+/**
+ * @brief Define a cor da borda do círculo.
+ * 
+ * @param c o círculo com a cor da borda definido.
+ * @param corB Novo valor para a cor da borda definido.
+ */
+
+void setCorBCirculo(CIRCULO c, char* corB);
+
+/**
+ * @brief Libera a memória alocada para o círculo.
+ * @param c ponteiro do círculo a ser destruído.
+ */
+void destroiCirculo(CIRCULO c);
+
+#endif
